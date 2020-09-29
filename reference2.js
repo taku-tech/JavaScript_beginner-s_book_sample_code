6a43d88c1c0e22a1ed6eac4179e5109c229b6ecd
taku-tech <59305697+taku-tech@users.noreply.github.com>
2020-09-29 12:27:26 +0900

    リスト4-15

---
+++ reference2.js
@@ -0,0 +1,11 @@
+var value = [1, 2, 4, 8, 16];
+
+function decrementValue(value) {
+  value--;
+  return value;
+};
+
+console.log(decrementValue(value));//①
+console.log(value);//②
+// 参照型は参照元の値を変更させるので仮引数がローカル変数だとしても
+// ①でグローバル変数を仮引数に入れているので、②も同じ値に変更される。
